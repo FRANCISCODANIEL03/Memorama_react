b3e51b626b5473a055c0e9fdb9dc13cb1ae7eac6
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-13 22:06:54 -0600

    refactor App component to include data fetching and error handling

--- proyecto2_react/src/App.jsx
+++ proyecto2_react/src/App.jsx
@@ -3,10 +3,21 @@ import Users from "./usuarios/Users";
 
 const URL = "https://jsonplaceholder.typicode.com/users"
 
-const App = ()=>{
+const App = () => {
   const [userList, setUserList] = useState([])
   const [filterList, setFilterList] = useState([])
 
+  const extractData = async () => {
+    try {
+      const response = await fetch(URL)
+      const usersData = await response.json()
+      setUserList(usersData)
+    } catch (error) {
+      alert("No se puede acceder al servidor en este momento")
+      console.error(error.message);
+      setUserList([]);
+    }
+  }
 }
 
 export default App
