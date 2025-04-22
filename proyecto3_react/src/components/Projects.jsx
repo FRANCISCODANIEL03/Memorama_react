6eca6b08b82fcce64b06a487348adb6b723b50c5
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-21 21:04:13 -0600

    add section and title

---
+++ proyecto3_react/src/components/Projects.jsx
@@ -0,0 +1,12 @@
+const Projects = () => {
+    return (
+        <section id="projects" className="projects">
+            <span className="tit"><i className="bi bi-file-earmark-bar-graph"></i> Mis Proyectos</span>
+            <div className="projects-div">
+                    
+            </div>
+        </section>
+    );
+};
+
+export default Projects;
