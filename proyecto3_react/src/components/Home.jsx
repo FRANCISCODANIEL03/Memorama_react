56b076d08dd524cfabd7dc4d4281b10d5b35112d
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-21 21:02:40 -0600

    add div with the presentation

--- proyecto3_react/src/components/Home.jsx
+++ proyecto3_react/src/components/Home.jsx
@@ -1,7 +1,16 @@
 const Home = ()=>{
     return(
         <section id="home" className="home">
-            
+            <img src="/imgs/image.jpg" alt="Foto de perfil" />
+            <div id="desc">
+                <span className="tit"><i className="bi bi-lightning-charge"></i> Bienvenido</span>
+                <p>¡Hola! Mi nombre es Francisco Daniel Gregorio Guevara, un apasionado desarrollador
+                    con experiencia en Pyhton, Javascript, Node.js, Mysql, PostgreSQL y C. En este espacio 
+                    encontrarás una muestra de mis proyectos, mis habilidades y un poco más sobre mí.
+                    Me encanta crear soluciones innovadoras y funcionales, ya sea a través del desarrollo 
+                    web, el diseño de interfaces atractivas o la optimización de sistemas. 
+                   Si te interesa, ¡contactame para colaborar juntos en futuras oportunidades!</p>
+            </div>
         </section>
     )
 }
