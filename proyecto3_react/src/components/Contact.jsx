08aebe247bb253d8e9b7053a5951afce78ba51dc
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-22 21:24:21 -0600

    add Contact information

--- proyecto3_react/src/components/Contact.jsx
+++ proyecto3_react/src/components/Contact.jsx
@@ -1,7 +1,14 @@
 const Contact = () => {
     return (
         <footer id="contact" className="contact">
-            
+            <div className="contact-info">
+            <span><i className="bi bi-person-lines-fill"></i> Contacto</span>
+                <div className="info">
+                <p><i className="bi bi-telephone"></i> 5543234522</p>
+                <p><i className="bi bi-envelope"></i> francisco23#@gmail.com</p>
+                </div>
+                
+            </div>
         </footer>
     );
 };
