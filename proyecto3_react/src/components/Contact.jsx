74a444dbaf81f8e7ee1af60a4dcaf5171a0f121e
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-23 21:25:15 -0600

    add social links section to Contact component

--- proyecto3_react/src/components/Contact.jsx
+++ proyecto3_react/src/components/Contact.jsx
@@ -11,6 +11,8 @@ const Contact = () => {
                     <a href="https://www.linkedin.com/in/francisco-daniel-gregorio-guevara-383b1a353?
                     utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" 
                     target="_blank"><i className="bi bi-linkedin"></i> linkedIn</a>
+                    <a href="https://github.com/FRANCISCODANIEL03" 
+                    target="_blank"><i class="bi bi-github"></i> GitHub</a>
                     
                 </div>
             </div>
