451ce443b98aeaa2f145f11357c47f7d69766b40
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-25 21:34:08 -0600

    add users

---
+++ proyecto5_react/src/context/AppContext.jsx
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+
+export const AppContext = createContext()
+
+const Users = [
+    { id: 1, name: "Emiliano" },
+    { id: 2, name: "Jordi" },
+    { id: 3, name: "Max" },
+]
+  
