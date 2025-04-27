a6d4937c6e84b1d0881dcb7be3afcf4835c75e19
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-26 21:38:12 -0600

    fix formatting in Selector component for consistency

--- proyecto5_react/src/components/Selector.jsx
+++ proyecto5_react/src/components/Selector.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 
-const Selector = ()=>{
+const Selector = () => {
     const { Users, setCurrentUserId } = useContext(AppContext)
 
     return (
         <ul className="my-5">
             {
-                Users.map((user, i) =>(
+                Users.map((user, i) => (
                     <li key={i} className="bg-blue-200 my-3 p-3 rounded-md font-bold text-lg hover:bg-blue-500 shadow-lg shadow-blue-500/50"
-                    onClick={()=>setCurrentUserId(user.id)}>
+                        onClick={() => setCurrentUserId(user.id)}>
                         <button>
                             {user.name}
                         </button>
