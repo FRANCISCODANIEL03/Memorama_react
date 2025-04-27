3abb00fb7974eeff9eaced33cb33f64e18125b64
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-26 21:37:55 -0600

    add users

--- proyecto5_react/src/components/Selector.jsx
+++ proyecto5_react/src/components/Selector.jsx
@@ -6,7 +6,16 @@ const Selector = ()=>{
 
     return (
         <ul className="my-5">
-            
+            {
+                Users.map((user, i) =>(
+                    <li key={i} className="bg-blue-200 my-3 p-3 rounded-md font-bold text-lg hover:bg-blue-500 shadow-lg shadow-blue-500/50"
+                    onClick={()=>setCurrentUserId(user.id)}>
+                        <button>
+                            {user.name}
+                        </button>
+                    </li>
+                ))
+            }
         </ul>
     )
 }
