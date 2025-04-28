827f763105c7df1d6cc6038f62c59caab2860ab3
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-27 21:40:16 -0600

    add tasks

--- proyecto5_react/src/components/TaskList.jsx
+++ proyecto5_react/src/components/TaskList.jsx
@@ -9,7 +9,13 @@ const TaskList = ()=>{
         <main className="bg-blue-100 rounded-lg w-1/4 max-sm:w-3/4 flex flex-col items-center shadow-lg shadow-blue-500/50">
             <span className="font-bold text-2xl">Lista de tareas</span>
             <ul>
-                
+                {   tasks.length == 0 ?
+                    <li className="bg-red-500 w-auto p-5 rounded-lg text-lg font-bold my-3">No hay tareas</li>
+                    : 
+                    tasks.map((task, i)=>{
+                        return (<TaskItem key={i} task={task}/>)
+                    })
+                }
             </ul>
         </main>
     )
