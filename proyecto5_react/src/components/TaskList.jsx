b512f1ea4923694998d69616d11af44b072d34f6
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-27 21:40:34 -0600

    fix formatting in TaskList component for consistency

--- proyecto5_react/src/components/TaskList.jsx
+++ proyecto5_react/src/components/TaskList.jsx
@@ -2,18 +2,18 @@ import React, { useContext } from 'react'
 import TaskItem from './TaskItem'
 import { AppContext } from '../context/AppContext'
 
-const TaskList = ()=>{
+const TaskList = () => {
     const { tasks } = useContext(AppContext)
 
     return (
         <main className="bg-blue-100 rounded-lg w-1/4 max-sm:w-3/4 flex flex-col items-center shadow-lg shadow-blue-500/50">
             <span className="font-bold text-2xl">Lista de tareas</span>
             <ul>
-                {   tasks.length == 0 ?
+                {tasks.length == 0 ?
                     <li className="bg-red-500 w-auto p-5 rounded-lg text-lg font-bold my-3">No hay tareas</li>
-                    : 
-                    tasks.map((task, i)=>{
-                        return (<TaskItem key={i} task={task}/>)
+                    :
+                    tasks.map((task, i) => {
+                        return (<TaskItem key={i} task={task} />)
                     })
                 }
             </ul>
