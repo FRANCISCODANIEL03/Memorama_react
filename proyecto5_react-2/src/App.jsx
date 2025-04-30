f4a66cfebbcb6e605e25ae74d6b467441edb5f90
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-04-30 02:30:14 -0600

    add users

--- proyecto5_react-2/src/App.jsx
+++ proyecto5_react-2/src/App.jsx
@@ -2,3 +2,9 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Selector from "./components/Selector";
 import TaskList from "./components/TaskList";
+
+const Users = [
+  { id: 1, name: "Emiliano" },
+  { id: 2, name: "Jordi" },
+  { id: 3, name: "Max" },
+]
