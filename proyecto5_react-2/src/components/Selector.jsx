f9a4f927c04489e045a4d8de516bff1f3be14d1d
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-05-02 02:33:26 -0600

    refactor Selector component to render user list with click functionality

--- proyecto5_react-2/src/components/Selector.jsx
+++ proyecto5_react-2/src/components/Selector.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const Selector = ({listUsers, setCurrentUserId})=>{
+    return (
+        <ul className="my-5">
+            {
+                listUsers.map((user, i) =>(
+                    <li key={i} className="bg-blue-200 my-3 p-3 rounded-md font-bold text-lg hover:bg-blue-500 shadow-lg shadow-blue-500/50"
+                    onClick={()=>setCurrentUserId(user.id)}>
+                        <button>
+                            {user.name}
+                        </button>
+                    </li>
+                ))
+            }
+        </ul>
+    )
 }
 
 export default Selector
