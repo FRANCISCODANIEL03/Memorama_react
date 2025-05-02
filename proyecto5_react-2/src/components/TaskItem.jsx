67fb9445b08db2027887eeddbc41a1c52e7ff931
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-05-02 02:33:59 -0600

    add TaskItem component to display individual tasks

---
+++ proyecto5_react-2/src/components/TaskItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskItem = ({task})=>{
+    console.log(task)
+    return (
+        <li className="bg-teal-300 w-auto p-5 rounded-lg text-lg font-bold my-3">
+            {
+             task
+            }
+        </li>
+    )
+}
+
+export default TaskItem
