54fbacdc9448a701e9aa18ce488e947755992efd
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-05-03 02:35:18 -0600

    refactor TaskList component to return JSX structure for task display

--- proyecto5_react-2/src/components/TaskList.jsx
+++ proyecto5_react-2/src/components/TaskList.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import TaskItem from './TaskItem'
 
 const TaskList = ({tasks})=>{
-   
+    return (
+        <main className="bg-green-200 rounded-lg w-1/4 flex flex-col items-center shadow-lg shadow-green-500/50">
+            <span className="font-bold text-2xl">Lista de tareas</span>
+            <ul>
+                {
+                    tasks.map((task, i)=>{
+                        return (<TaskItem key={i} task={task}/>)
+                    })
+                }
+            </ul>
+        </main>
+    )
 }
 
