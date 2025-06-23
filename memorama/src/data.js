dba55364c248fd3fb64d702803a940c98766403f
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-06-22 19:13:47 -0600

    add additional image data for Memorama game

--- memorama/src/data.js
+++ memorama/src/data.js
@@ -23,5 +23,10 @@ export const imgs = [
         id: 5,
         img: "/imgs/mano-der.png",
         alt: "Mano derecha"
+    },
+    {
+        id: 6,
+        img: "/imgs/mano-izq.png",
+        alt: "Mano izquierda"
     }
 ]
