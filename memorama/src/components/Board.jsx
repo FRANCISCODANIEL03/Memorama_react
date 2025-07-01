6027fbcaa38d957c321c620f9b72879208465234
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-06-30 19:27:51 -0600

    feat: add shuffleArray function to randomize array elements

--- memorama/src/components/Board.jsx
+++ memorama/src/components/Board.jsx
@@ -2,3 +2,11 @@ import React, { useEffect, useState } from "react";
 import { Cards } from "./Cards";
 import { imgs } from "../data";
 import { Modal } from "./Modal";
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
