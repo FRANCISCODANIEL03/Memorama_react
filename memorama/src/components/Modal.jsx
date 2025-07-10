57b2ecedd64031af80f708b19d2afa8867bf8c12
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-07-09 19:43:16 -0600

    completing the code

--- memorama/src/components/Modal.jsx
+++ memorama/src/components/Modal.jsx
@@ -5,4 +5,12 @@ export const Modal = ({ gameOver, setGameOver, handlenewGame, scores }) => {
         return "¡Empate!";
     }
 
+    return (
+        <div className={`${gameOver ? "flex" : "hidden"} flex-col justify-center items-center gap-7
+         bg-white absolute w-[320px] h-[320px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-40 rounded-lg`}>
+            <button className="text-black font-bold absolute right-0 top-0 mr-3 hover:text-yellow-300 text-2xl" 
+            onClick={() => setGameOver(false)}>&times;</button>
+
+        </div>
+    );
 }
