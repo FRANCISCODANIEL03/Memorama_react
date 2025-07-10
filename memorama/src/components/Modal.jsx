7d3a6e918153f2426e7cc71119ff1398dfcb8f64
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-07-09 19:42:54 -0600

    completing the code

--- memorama/src/components/Modal.jsx
+++ memorama/src/components/Modal.jsx
@@ -1,2 +1,8 @@
 export const Modal = ({ gameOver, setGameOver, handlenewGame, scores }) => {
+    const winnerText = () => {
+        if (scores[1] > scores[2]) return "¡Ganó Jugador 1!";
+        if (scores[2] > scores[1]) return "¡Ganó Jugador 2!";
+        return "¡Empate!";
+    }
+
 }
