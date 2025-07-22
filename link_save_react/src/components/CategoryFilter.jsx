0058f5b24ebff4cdda477ff98360bf495d9e6efe
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-07-21 20:14:17 -0600

    completing the code

--- link_save_react/src/components/CategoryFilter.jsx
+++ link_save_react/src/components/CategoryFilter.jsx
@@ -6,6 +6,13 @@ export function CategoryFilter() {
   return (
     <div className='flex flex-col md:flex-row justify-between items-center mx-8 mb-4 gap-4'>
       <div>
+        <label className='mr-2 font-semibold'>Filtrar por categoría:</label>
+        <select onChange={(e) => setFilter(e.target.value)} className='border-b-2 border-indigo-500 p-1'>
+          <option value="Todas">Todas</option>
+          {categories.map((cat, i) => (
+            <option key={i} value={cat}>{cat}</option>
+          ))}
+        </select>
       </div>
     </div>
   );
