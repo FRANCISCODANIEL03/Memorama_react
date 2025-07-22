6627a3ab5f00ceead873a5edaf386f209b95d6c5
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-07-21 20:14:07 -0600

    completing the code

--- link_save_react/src/components/CategoryFilter.jsx
+++ link_save_react/src/components/CategoryFilter.jsx
@@ -3,4 +3,10 @@ import { useVideoContext } from '../context/VideoContext';
 export function CategoryFilter() {
   const { categories, setFilter, searchText, setSearchText } = useVideoContext();
 
+  return (
+    <div className='flex flex-col md:flex-row justify-between items-center mx-8 mb-4 gap-4'>
+      <div>
+      </div>
+    </div>
+  );
 }
