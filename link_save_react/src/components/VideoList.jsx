1b95406fce68f152ed61bdbd5d80c8e071e02701
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-07-28 20:12:32 -0600

    add rendering for video items in VideoList component

--- link_save_react/src/components/VideoList.jsx
+++ link_save_react/src/components/VideoList.jsx
@@ -18,6 +18,10 @@ export function VideoList() {
 
   return (
     <div className='mx-8 mt-6 space-y-4'>
+      {filteredVideos.map((video, index) => (
+        <div key={index} className='bg-white shadow-md rounded-lg p-4 flex justify-between items-center'>
+        </div>
+      ))}
     </div>
   );
 }
