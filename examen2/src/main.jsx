ebd7e5ecedc54e03812d9e14c19e19d8f234161f
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-08-01 20:17:38 -0600

    complete file

--- examen2/src/main.jsx
+++ examen2/src/main.jsx
@@ -3,3 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import "bootstrap-icons/font/bootstrap-icons.min.css"
 import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
