debc197ad05b5c81e778b7aa038cc8f4a66ce396
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-08-21 21:14:01 -0600

    add return

--- examen2/src/components/LibroFilter.jsx
+++ examen2/src/components/LibroFilter.jsx
@@ -3,4 +3,6 @@ import { useLibroContext } from '../context/LibroContext';
 export function LibroFilter() {
   const { generos, setFilter, searchText, setSearchText } = useLibroContext();
 
+  return (
+  );
 }
