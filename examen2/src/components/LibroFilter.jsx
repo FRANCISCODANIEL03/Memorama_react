562d49b51bcbceb343c9b7f7bd820e2556ded5c0
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-08-22 21:14:19 -0600

    complete return

--- examen2/src/components/LibroFilter.jsx
+++ examen2/src/components/LibroFilter.jsx
@@ -4,5 +4,7 @@ export function LibroFilter() {
   const { generos, setFilter, searchText, setSearchText } = useLibroContext();
 
   return (
+    <div className='flex flex-col md:flex-row justify-between items-center mx-8 mb-4 gap-4'>
+    </div>
   );
 }
