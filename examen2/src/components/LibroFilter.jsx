3c4665dbc5904aac06281201004e337a71ccf4fc
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-08-22 21:14:36 -0600

    complete return

--- examen2/src/components/LibroFilter.jsx
+++ examen2/src/components/LibroFilter.jsx
@@ -5,6 +5,8 @@ export function LibroFilter() {
 
   return (
     <div className='flex flex-col md:flex-row justify-between items-center mx-8 mb-4 gap-4'>
+      <div>
+      </div>
     </div>
   );
 }
