9a9b0fcd5a99e689540955b4808ae33caa303475
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-08-27 21:25:19 -0600

    complete return

--- examen2/src/components/LibroList.jsx
+++ examen2/src/components/LibroList.jsx
@@ -18,6 +18,10 @@ export function LibroList() {
 
   return (
     <div className='mx-8 mt-6 space-y-4'>
+      {filteredLibros.map((video, index) => (
+        <div key={index} className='bg-white shadow-md rounded-lg p-4 flex justify-between items-center'>
+        </div>
+      ))}
     </div>
   );
 }
