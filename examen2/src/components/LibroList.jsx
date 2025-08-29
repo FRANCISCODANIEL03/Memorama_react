5c8aac517a5b8f158f2a8142f9d6046487ef3db0
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-08-28 21:27:32 -0600

    complete return

--- examen2/src/components/LibroList.jsx
+++ examen2/src/components/LibroList.jsx
@@ -22,6 +22,7 @@ export function LibroList() {
         <div key={index} className='bg-white shadow-md rounded-lg p-4 flex justify-between items-center'>
           <div className='text-left'>
             <p className='text-lg font-bold'>{highlightMatch(video.title)}</p>
+            <p className='text-sm text-gray-600'>{video.genero}</p>
           </div>
         </div>
       ))}
