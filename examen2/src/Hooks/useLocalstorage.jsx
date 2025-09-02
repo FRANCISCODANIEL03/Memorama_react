668bf27afa19e79990df1d2db27bcd140e0c9ef1
FRANCISCODANIEL03 <franciscogreg0311@gmail.com>
2025-09-01 21:30:53 -0600

    add constant

--- examen2/src/Hooks/useLocalstorage.jsx
+++ examen2/src/Hooks/useLocalstorage.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 
 const useLocalStorage = (key, valorInicial) => {
+    const [valor, setValor] = useState(() =>{
+        const valorGuardado = localStorage.getItem(key)
+        return valorGuardado ? JSON.parse(valorGuardado) : valorInicial
+    })
+
 }
 
 export default useLocalStorage
